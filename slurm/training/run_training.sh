54ed7e54ab4f8b73db0a74da5f0e1b1dd1883d8b
Mahdi <mbd7793@gmail.com>
2025-08-12 20:41:42 -0500

    save latest changes

--- slurm/training/run_training.sh
+++ slurm/training/run_training.sh
@@ -6,16 +6,22 @@
 #SBATCH --ntasks=1                   
 #SBATCH --gres=gpu:1                
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=900G
+#SBATCH --mem=256G
 #SBATCH --time=10:00:00
 #SBATCH --output=logs/slurm/training_100K_%j.out
 #SBATCH --error=logs/slurm/training_100K_%j.err
 
 
 # Set paths
-SIMILARITY_PT="/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/data/1M/similarity_output_0.75/similarity_graph_20250812_043913.pt"
-SIMILARITY_NPZ="/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/data/1M/similarity_output_0.75/similarity_graph_20250812_043913.npz"
-FEATURES_CSV="/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/data/1M/aiio_sample_1000000_normalized.csv"
+# 1M dataset paths
+# SIMILARITY_PT="/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/data/1M/similarity_output_0.75/similarity_graph_20250812_043913.pt"
+# SIMILARITY_NPZ="/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/data/1M/similarity_output_0.75/similarity_graph_20250812_043913.npz"
+# FEATURES_CSV="/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/data/1M/aiio_sample_1000000_normalized.csv"
+
+# 100K dataset paths
+SIMILARITY_PT="/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/data/100K/similarity_output_0.75/similarity_graph_20250812_002240.pt"
+SIMILARITY_NPZ="/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/data/100K/similarity_output_0.75/similarity_graph_20250812_002240.npz"
+FEATURES_CSV="/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/data/100K/aiio_sample_100000_normalized.csv"
 
 # Create experiment directory with timestamp
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
@@ -24,14 +30,18 @@ SAVE_DIR="./experiments/gat_exp_${TIMESTAMP}"
 # Create the directory first
 mkdir -p "$SAVE_DIR"
 
+# Set Config path
+CONFIG="/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/configs/gat_config2.yaml"
+
 # Run training
 python scripts/train_gat.py \
     --similarity-pt "$SIMILARITY_PT" \
     --similarity-npz "$SIMILARITY_NPZ" \
     --features-csv "$FEATURES_CSV" \
-    --epochs 200 \
-    --lr 0.001 \
-    --seed 42 \
+    --config "$CONFIG" \
+    # --epochs 200 \
+    # --lr 0.001 \
+    # --seed 42 \
     --save-dir "$SAVE_DIR" \
     --run-name "gat_100k_${TIMESTAMP}" \
     --force-mode mini \
