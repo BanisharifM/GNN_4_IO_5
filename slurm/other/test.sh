1a37accc9efd51bd552e15563abe588c5fe10593
Mahdi <mbd7793@gmail.com>
2025-08-15 05:03:51 -0500

    save latest chagnes

--- slurm/other/test.sh
+++ slurm/other/test.sh
@@ -8,8 +8,8 @@
 #SBATCH --cpus-per-task=32
 #SBATCH --mem=128G
 #SBATCH --time=01:00:00
-#SBATCH --output=logs/slurm/darshan/job_processor_simple_%j.out
-#SBATCH --error=logs/slurm/darshan/job_processor_simple_%j.err
+#SBATCH --output=logs/slurm/darshan/ior_bottleneck_analysis_%j.out
+#SBATCH --error=logs/slurm/darshan/ior_bottleneck_analysis_%j.err
 
 
-srun python /work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/scripts/test_job_processor_simple.py
+srun python /work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/scripts/ior/ior_bottleneck_analysis.py
