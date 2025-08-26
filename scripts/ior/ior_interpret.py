44fe7d306f014e83d7da8f7918e105dc35b4790d
Mahdi <mbd7793@gmail.com>
2025-08-25 22:55:00 -0500

    adding E2E darshan log

--- scripts/ior/ior_interpret.py
+++ scripts/ior/ior_interpret.py
@@ -1286,7 +1286,7 @@ def main():
         logger.warning("⚠ Training features not found")
     
     # IOR test sample
-    test_features = '/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/darshan_features_ior_normalized_baseline_read.csv'
+    test_features = '/work/hdd/bdau/mbanisharifdehkordi/GNN_4_IO_5/darshan_features_e2e_pathological_11642213_64procs_1stripe_64kb.csv'
     
     # Initialize analyzer
     logger.info("\n" + "="*70)
--- slurm/other/test.sh
+++ slurm/other/test.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=data_sampeling
 #SBATCH --account=bdau-delta-gpu
-#SBATCH --partition=gpuH200x8-interactive
+#SBATCH --partition=gpuA100x4-interactive
 #SBATCH --nodes=1                     
 #SBATCH --ntasks=1                   
 #SBATCH --gres=gpu:1                
 #SBATCH --cpus-per-task=32
-#SBATCH --mem=128G
+#SBATCH --mem=32G
 #SBATCH --time=01:00:00
 #SBATCH --output=logs/slurm/darshan/ior_interpret_%j.out
 #SBATCH --error=logs/slurm/darshan/ior_interpret_%j.err
